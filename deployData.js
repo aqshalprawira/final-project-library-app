8b23047385f8e2188e0a2f6f7e8e01696a54d2cd
aqshalprawira <aqshal.map@gmail.com>
2023-05-23 15:52:48 +0700

    tugas final project library app

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Muhammad Aqshal Prawira'; // TODO: replace this
+
+const studentId = 'FE5622157'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
